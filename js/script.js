6f10c092f38517594fbf4cb0349006c9a56e4937
unknown <anikolenko@stud.khpi.ucode-connect.study>
2022-02-20 22:57:10 +0200

    fd

--- js/script.js
+++ js/script.js
@@ -7,15 +7,20 @@ let result = ""
 for (let i = 0; i < el.length; i++) {
     let number = ''
     let count = 0
+    let done = ''
     el[i].addEventListener("click", () => {
         currentButton = el[i].textContent
         console.log(currentButton)
         if (currentButton == 'C') {
             count = 0
             input = document.getElementById("inputField").innerHTML = ""
+            done = document.getElementById("result_text").innerHTML = ""
+            console.log("done ok")
         }
         else if (currentButton == '=') {
-            let done = doTheThing(input);
+            done = ''
+            if (input != '')
+                done = doTheThing(input);
             result = document.getElementById("result_text").innerHTML = done
         }   
         else {
@@ -25,12 +30,56 @@ for (let i = 0; i < el.length; i++) {
 }
 
 function doTheThing(input) {
+    let res = 0;
     let temp = input.split(' ')
-    if (temp[1] == '+') {
-        console.log(Number(temp[0]) + Number(temp[2]))
-        let res = Number(temp[0]) + Number(temp[2])
-        return res
+    if (!temp) return ""
+    console.log(temp)
+    for (let e = 1; e < Number(temp.length) - 1; e += 2) {
+        if (e == 1){
+            if (temp[e] == '+') {
+                console.log(res)
+                res += Number(temp[e - 1]) + Number(temp[e + 1])
+            }
+            else if (temp[e] == '-') {
+                console.log(res)
+                res += Number(temp[e - 1]) - Number(temp[e + 1])
+            }
+            else if (temp[e] == '*') {
+                console.log(res)
+                res += Number(res) * Number(temp[e + 1])
+            }
+            else if (temp[e] == '÷') {
+                console.log(res)
+                res += Number(res) / Number(temp[e + 1])
+            }
+            else if (temp[e] == '%') {
+                console.log(res)
+                res += Number(res) % Number(temp[e + 1])
+            }
+        }
+        else{
+            if (temp[e] == '+') {
+                console.log(res)
+                res = Number(res) + Number(temp[e + 1])
+            }
+            else if (temp[e] == '-') {
+                console.log(res)
+                res = Number(res) - Number(temp[e + 1])
+            }
+            else if (temp[e] == '*') {
+                console.log(res)
+                res = Number(res) * Number(temp[e + 1])
+            }
+            else if (temp[e] == '÷') {
+                console.log(res)
+                res = Number(res) / Number(temp[e + 1])
+            }
+            else if (temp[e] == '%') {
+                console.log(res)
+                res = Number(res) % Number(temp[e + 1])
+            }
+        }
     }
+    return res
     
 }
-
